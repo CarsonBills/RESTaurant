6a73831b98497ab8965534fcf7e27d6b124da51e
CarsonBills <CarsonMBills@Gmail.com>
2014-10-14 14:26:08 -0400

    Adds New menu item functionality

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'models/food'
+require_relative 'models/party'
+
 ActiveRecord::Base.establish_connection({
 	adapter: 'postgresql',
 	database: 'restaurant'
@@ -8,4 +11,18 @@ ActiveRecord::Base.establish_connection({
 
 get '/' do
 	erb :index
+end
+
+get '/foods' do
+	@foods = Food.all
+	erb :'foods/index'
+end
+
+get '/foods/new' do
+	erb :'foods/new'
+end
+
+post '/foods' do
+	Food.create(params[:food])
+	redirect '/foods'
 end
